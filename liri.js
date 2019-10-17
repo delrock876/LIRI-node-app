cd3d07b060b20e36827b43689eef96875fa2f647
Sabrina Rocha <delrock876@gmail.com>
2019-10-17 14:11:36 -0700

    set up complete

--- liri.js
+++ liri.js
@@ -1,3 +1,17 @@
 require("dotenv").config()
 
 const keys = require("./keys.js")
+const moment = require('moment')
+const axios = require('axios')
+const fs = require('fs')
+const spotify = new Spotify(keys.spotify)
+
+
+// Make it so liri.js can take in one of the following commands:
+//   * `concert-this`
+
+//   * `spotify-this-song`
+
+//   * `movie-this`
+
+//   * `do-what-it-says`
