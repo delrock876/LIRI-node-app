5e1c4bab42065166beac08fd969aa269ca7b960f
Sabrina Rocha <delrock876@gmail.com>
2019-10-24 18:36:08 -0700

    created array from random.txt

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ let command = process.argv[2]
 
 //   Search concert
 const concertThis = _ => {
-  artists = process.argv[3]
+  let artists = process.argv[3]
   axios.get(`https://rest.bandsintown.com/artists/${artists}/events?app_id=codingbootcamp`)
     .then(({ data }) => {
       if (data.length === 0) {
@@ -31,7 +31,7 @@ const concertThis = _ => {
 
 //  Search spotify
 const spotifyThis = _ => {
-  songTitle = process.argv[3]
+  let songTitle = process.argv[3]
   spotify.search({ type: 'track', query: `${songTitle}`, limit: 3 })
     .then(({ tracks: { items } }) => {
       if (items.length === 0) {
@@ -49,27 +49,25 @@ const spotifyThis = _ => {
 }
 
 //movie this
-
 const movieThis = _ => {
-  movie = process.argv[3]
+  let movie = process.argv[3]
   axios.get(`https://www.omdbapi.com/?t=${movie}&apikey=trilogy&`)
     .then(({ data }) => {
       if (movie) {
         console.log(`
-  Title: ${data.Title}
-  Release Year:  ${data.Year}
-  IMBD Rating:  ${data.Ratings[0].Value}
-  Rotten Tomatoes Rating:  ${data.Ratings[1].Value}
-  Country Produced:  ${data.Country}
-  Language:  ${data.Language}
-  Actors:  ${data.Actors}
-  Plot:
-  ${data.Plot}`)
-        //   //    ```
+        Title: ${data.Title}
+        Release Year:  ${data.Year}
+        IMBD Rating:  ${data.Ratings[0].Value}
+        Rotten Tomatoes Rating:  ${data.Ratings[1].Value}
+        Country Produced:  ${data.Country}
+        Language:  ${data.Language}
+        Actors:  ${data.Actors}
+        Plot:
+        ${data.Plot}`)
+     
       } else {
         axios.get(`https://www.omdbapi.com/?t=Cats&apikey=trilogy&`)
           .then(({ data }) => {
-            // console.log(data)
             console.log(
               `You should watch Cats! Check it out here: https://www.imdb.com/title/${data.imdbID}/`)
             console.log('It may or not be on Netflix!')
@@ -78,6 +76,24 @@ const movieThis = _ => {
       }
     })
 }
+
+
+const doThis =_=>{
+fs.readFile('random.txt', 'utf8', (e, data) => {
+let str = data
+let newStr =  str.split(`,`)
+ console.log(newStr)
+ newStr[0] = process.argv[2]
+ newStr[1] = process.argv[3]
+spotifyThis()
+
+})
+// for(let i = 0; i<)
+// fs.readFile('file.txt', 'utf8', (e, data) => {console.log(e ? e : data)})
+
+}
+
+
 
 switch (command) {
 
@@ -91,6 +107,10 @@ switch (command) {
 
   case 'movie-this':
     movieThis()
+    break
+
+  case 'do-this':
+    doThis()
     break
 }
 
